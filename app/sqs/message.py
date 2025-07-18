0d41a388813fb83f8c0b36eaa4b56f5f3f5ba2ce
Thu Nguyen <thunguy@me.com>
2025-07-18 14:12:02 -0700

    Initial commit for SQS implementation

---
+++ app/sqs/message.py
@@ -0,0 +1,60 @@
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+logger = logging.getLogger(__name__)
+sqs = boto3.resource("sqs")
+
+
+def receive_messages(queue, max_number, wait_time):
+    """
+    Receive a batch of messages in a single request from an SQS queue.
+
+    :param queue: The queue from which to receive messages.
+    :param max_number: The maximum number of messages to receive. The actual number
+                       of messages received might be less.
+    :param wait_time: The maximum time to wait (in seconds) before returning. When
+                      this number is greater than zero, long polling is used. This
+                      can result in reduced costs and fewer false empty responses.
+    :return: The list of Message objects received. These each contain the body
+             of the message and metadata and custom attributes.
+    """
+    try:
+        messages = queue.receive_messages(
+            MessageAttributeNames=["All"],
+            MaxNumberOfMessages=max_number,
+            WaitTimeSeconds=wait_time,
+        )
+        for msg in messages:
+            logger.info("Received message: %s: %s", msg.message_id, msg.body)
+    except ClientError as error:
+        logger.exception("Couldn't receive messages from queue: %s", queue)
+        raise error
+    else:
+        return messages
+
+
+def send_message(queue, message_body, message_attributes=None):
+    """
+    Send a message to an Amazon SQS queue.
+
+    :param queue: The queue that receives the message.
+    :param message_body: The body text of the message.
+    :param message_attributes: Custom attributes of the message. These are key-value
+                               pairs that can be whatever you want.
+    :return: The response from SQS that contains the assigned message ID.
+    """
+    if not message_attributes:
+        message_attributes = {}
+
+    try:
+        response = queue.send_message(
+            MessageBody=message_body, MessageAttributes=message_attributes
+        )
+    except ClientError as error:
+        logger.exception("Send message failed: %s", message_body)
+        raise error
+    else:
+        return response
